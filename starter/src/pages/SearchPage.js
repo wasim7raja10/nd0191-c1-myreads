850375c64a81355d2513125881b4a18e63b935c4
Wasim Raja <wasim7raja10@gmail.com>
2022-11-24 01:28:43 +0530

    style: formatting

--- starter/src/pages/SearchPage.js
+++ starter/src/pages/SearchPage.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SearchPage = () => {
   return (
     <div className="search-books">
       <div className="search-books-bar">
-        <Link
-          className="close-search"
-          to={'/'}
-        >
+        <Link className="close-search" to={"/"}>
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            placeholder="Search by title, author, or ISBN"
-          />
+          <input type="text" placeholder="Search by title, author, or ISBN" />
         </div>
       </div>
       <div className="search-books-results">
         <ol className="books-grid"></ol>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
