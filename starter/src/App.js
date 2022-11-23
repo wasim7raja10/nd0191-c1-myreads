1c5da5f119517841bf8e94d0a989c2f6523b47ea
Wasim Raja <wasim7raja10@gmail.com>
2022-11-24 02:29:10 +0530

    feat: Fetch all books and stored them in their respective shelves

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,11 +2,43 @@ import "./App.css";
 import ListBooks from "./pages/ListBooks";
 import SearchPage from "./pages/SearchPage";
 import { Route, Routes } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import React from "react";
 
 function App() {
+  const [wantToRead, setWantToRead] = React.useState([]);
+  const [currentlyReading, setCurrentlyReading] = React.useState([]);
+  const [read, setRead] = React.useState([]);
+
+  React.useEffect(() => {
+    const getAllBooks = async () => {
+      const allBooks = await BooksAPI.getAll();
+      allBooks.forEach((books) => {
+        if (books.shelf === "wantToRead") {
+          setWantToRead((prev) => [...prev, books]);
+        } else if (books.shelf === "currentlyReading") {
+          setCurrentlyReading((prev) => [...prev, books]);
+        } else if (books.shelf === "read") {
+          setRead((prev) => [...prev, books]);
+        }
+      });
+    };
+    getAllBooks();
+  }, []);
+
   return (
     <Routes className="app">
-      <Route exact path="/" element={<ListBooks />} />
+      <Route
+        exact
+        path="/"
+        element={
+          <ListBooks
+            wantToRead={wantToRead}
+            currentlyReading={currentlyReading}
+            read={read}
+          />
+        }
+      />
       <Route path="/search" element={<SearchPage />} />
     </Routes>
   );
