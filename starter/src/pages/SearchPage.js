2e5312c75d3c5fe2e63e3e3dedca65a3c28426f9
Wasim Raja <wasim7raja10@gmail.com>
2022-11-24 03:59:30 +0530

    fix: add error handling on searching books

--- starter/src/pages/SearchPage.js
+++ starter/src/pages/SearchPage.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import Book from "../components/Book";
 
 const SearchPage = () => {
+  const [query, setQuery] = React.useState("");
+  const [books, setBooks] = React.useState([]);
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    if (query) {
+      BooksAPI.search(query).then((books) => {
+        setBooks(books);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }, [query])
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -9,11 +28,16 @@ const SearchPage = () => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input type="text" placeholder="Search by title, author, or ISBN" value={query}
+            onChange={handleChange} />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {books.error ? "No books found" : books.map((book) => (
+            <Book key={book.id} book={book} />
+          ))}
+        </ol>
       </div>
     </div>
   );
