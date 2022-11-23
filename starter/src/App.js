41abe9d24781bc1f367664c70cefd4e04873f34d
Wasim Raja <wasim7raja10@gmail.com>
2022-11-24 00:47:46 +0530

    refactor: remove unused code

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import { useState } from "react";
 import ListBooks from "./pages/ListBooks";
 import SearchPage from "./pages/SearchPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
-
   return (
     <Routes className="app">
       <Route exact path="/" element={<ListBooks />} />
