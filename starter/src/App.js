36d8cdbcfb36c5ea2f3d4a35fdf947da42fb92a2
Wasim Raja <wasim7raja10@gmail.com>
2022-11-24 03:56:27 +0530

    fix: shifted state from App to ListBooks

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,29 +2,10 @@ import "./App.css";
 import ListBooks from "./pages/ListBooks";
 import SearchPage from "./pages/SearchPage";
 import { Route, Routes } from "react-router-dom";
-import * as BooksAPI from "./BooksAPI";
 import React from "react";
 
 function App() {
-  const [wantToRead, setWantToRead] = React.useState([]);
-  const [currentlyReading, setCurrentlyReading] = React.useState([]);
-  const [read, setRead] = React.useState([]);
 
-  React.useEffect(() => {
-    const getAllBooks = async () => {
-      const allBooks = await BooksAPI.getAll();
-      allBooks.forEach((books) => {
-        if (books.shelf === "wantToRead") {
-          setWantToRead((prev) => [...prev, books]);
-        } else if (books.shelf === "currentlyReading") {
-          setCurrentlyReading((prev) => [...prev, books]);
-        } else if (books.shelf === "read") {
-          setRead((prev) => [...prev, books]);
-        }
-      });
-    };
-    getAllBooks();
-  }, []);
 
   return (
     <Routes className="app">
@@ -32,11 +13,7 @@ function App() {
         exact
         path="/"
         element={
-          <ListBooks
-            wantToRead={wantToRead}
-            currentlyReading={currentlyReading}
-            read={read}
-          />
+          <ListBooks />
         }
       />
       <Route path="/search" element={<SearchPage />} />
--- starter/src/pages/ListBooks.js
+++ starter/src/pages/ListBooks.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Book from "../components/Book";
+import * as BooksAPI from "../BooksAPI";
 
 const listOfShelves = ["Currently Reading", "Want to Read", "Read"];
 
-const ListBooks = ({ wantToRead, currentlyReading, read }) => {
+const ListBooks = () => {
+  const [wantToRead, setWantToRead] = React.useState([]);
+  const [currentlyReading, setCurrentlyReading] = React.useState([]);
+  const [read, setRead] = React.useState([]);
+
+  React.useEffect(() => {
+    const getAllBooks = async () => {
+      const allBooks = await BooksAPI.getAll();
+      allBooks.forEach((books) => {
+        if (books.shelf === "wantToRead") {
+          setWantToRead((prev) => [...prev, books]);
+        } else if (books.shelf === "currentlyReading") {
+          setCurrentlyReading((prev) => [...prev, books]);
+        } else if (books.shelf === "read") {
+          setRead((prev) => [...prev, books]);
+        }
+      });
+    };
+    getAllBooks();
+  }, []);
   return (
     <div className="list-books">
       <div className="list-books-title">
